2fbe32aed3a2717c33c2ebae75c5a9fd5a0dec98
AlphaKretin <deewhydeeecks@gmail.com>
2025-03-23 16:35:17 +1030

    Fix reversal form missing from form list

--- src/app/data/forms.ts
+++ src/app/data/forms.ts
@@ -4,22 +4,11 @@ import { dance } from "./forms/dance";
 import { iron } from "./forms/iron";
 import { onetwo } from "./forms/onetwo";
 import { power } from "./forms/power";
+import { reversal } from "./forms/reversal";
 import { shadow } from "./forms/shadow";
 import { song } from "./forms/song";
 import { thorns } from "./forms/thorns";
 import { vigilance } from "./forms/vigilance";
 import { wild } from "./forms/wild";
 
-export const forms = [
-    blaster,
-    control,
-    dance,
-    iron,
-    onetwo,
-    power,
-    shadow,
-    song,
-    thorns,
-    vigilance,
-    wild,
-];
+export const forms = [blaster, control, dance, iron, onetwo, power, reversal, shadow, song, thorns, vigilance, wild];
