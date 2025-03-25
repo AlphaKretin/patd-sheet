ada9a7b3a6d9594b6504000d2494f30127bb2e59
AlphaKretin <deewhydeeecks@gmail.com>
2025-03-25 22:33:55 +1030

    Add script to export hardcoded archetype data

---
+++ src/app/scripts/exportData.ts
@@ -0,0 +1,54 @@
+import * as fs from "fs";
+import * as path from "path";
+import { archetypes } from "../data/archetypes";
+import { forms } from "../data/forms";
+import { freestyles } from "../data/freestyles";
+import { Archetype } from "../data/types/Archetype";
+import { Form } from "../data/types/Form";
+import { Style } from "../data/types/Style";
+
+const outputDir = path.join(__dirname, "output");
+if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+}
+
+interface SavableArchetype extends Omit<Archetype, "styles"> {
+    styles: string[];
+}
+
+function exportData(folder: string, data: Array<SavableArchetype | Style | Form>) {
+    if (!fs.existsSync(path.join(outputDir, folder))) {
+        fs.mkdirSync(path.join(outputDir, folder));
+    }
+    data.forEach((dat) => {
+        const fileName = dat.name!.toLowerCase();
+        const filePath = path.join(outputDir, folder, `${fileName}.json`);
+        fs.writeFileSync(filePath, JSON.stringify(dat, null, 4));
+        console.log(`Saved ${fileName} to ${filePath}`);
+    });
+}
+
+exportData(
+    "archetypes",
+    archetypes.map((a) => {
+        const newArchetype: SavableArchetype = {
+            name: a.name,
+            focusedAbilities: a.focusedAbilities,
+            fusedAbilities: a.fusedAbilities,
+            franticAbilities: a.franticAbilities,
+            actions: a.actions,
+            styles: a.styles.map((s) => s.name.toLowerCase()),
+            alphaSuper: a.alphaSuper,
+            deltaSuper: a.deltaSuper,
+        };
+        return newArchetype;
+    })
+);
+if (!fs.existsSync(path.join(outputDir, "styles"))) {
+    fs.mkdirSync(path.join(outputDir, "styles"));
+}
+archetypes.forEach((a) => {
+    exportData(path.join("styles", a.name.toLowerCase()), a.styles);
+});
+exportData(path.join("styles", "free"), freestyles);
+exportData("forms", forms);
