fd7ae2f7e0738f97ad1b42c3ca4dc4e93b27e65f
AlphaKretin <deewhydeeecks@gmail.com>
2025-03-26 21:08:04 +1030

    Manually categories triggers
    For better sorting

--- src/app/data/types/Ability.ts
+++ src/app/data/types/Ability.ts
@@ -1,6 +1,17 @@
+type TriggerCategory =
+    | "after action"
+    | "after dice"
+    | "before action"
+    | "bleed"
+    | "character creation"
+    | "end of turn"
+    | "start of other turns"
+    | "start of your turn";
+
 export interface Ability {
     desc: string;
     trigger?: string;
+    triggerCategory: TriggerCategory;
     name?: string;
     bonusDice?: number[];
     bonusMaxRange?: number;
